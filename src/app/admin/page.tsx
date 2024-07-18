2b211522cb6d4cb9c715f5271a191ec5458435ab
Devanshu <design.devanshu@gmail.com>
2024-07-18 23:35:21 +0530

    refractor: update href in Link

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 export default function Admin() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <Link href="/users" className="rounded p-4 shadow dark:shadow-white">
+      <Link href="/admin/users" className="rounded p-4 shadow dark:shadow-white">
         User Management
       </Link>
-      <Link href="/analytics" className="rounded p-4 shadow dark:shadow-white">
+      <Link href="/admin/analytics" className="rounded p-4 shadow dark:shadow-white">
         Analytics
       </Link>
-      <Link href="/notifications" className="rounded p-4 shadow dark:shadow-white">
+      <Link href="/admin/notifications" className="rounded p-4 shadow dark:shadow-white">
         Notifications
       </Link>
-      <Link href="/settings" className="rounded p-4 shadow dark:shadow-white">
+      <Link href="/admin/settings" className="rounded p-4 shadow dark:shadow-white">
         Settings
       </Link>
-      <Link href="/tasks" className="rounded p-4 shadow dark:shadow-white">
+      <Link href="/admin/tasks" className="rounded p-4 shadow dark:shadow-white">
         Task Management
       </Link>
     </div>
